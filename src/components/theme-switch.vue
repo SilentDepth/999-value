091bee3e5ecbb7ca2165cdf9a567462727110900
SilentDepth <silentdepth@outlook.com>
2021-01-21 16:59:22 +0800

    Make dark mode transition more smooth

--- src/components/theme-switch.vue
+++ src/components/theme-switch.vue
@@ -19,7 +19,7 @@ button(class="fixed top-0 right-0 p-4 group" @click="dark = !dark")
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       fill="currentColor"
-      class="absolute inset-0 m-auto w-5 text-trueGray-400 group-hover:text-yellow-400 transition duration-100 ease-in-out transform-origin"
+      class="absolute inset-0 m-auto w-5 text-trueGray-400 group-hover:text-orange-400 transition duration-100 ease-in-out transform-origin"
     ): path(d="M12 18a6 6 0 1 1 0-12 6 6 0 0 1 0 12zM11 1h2v3h-2V1zm0 19h2v3h-2v-3zM3.515 4.929l1.414-1.414L7.05 5.636 5.636 7.05 3.515 4.93zM16.95 18.364l1.414-1.414 2.121 2.121-1.414 1.414-2.121-2.121zm2.121-14.85l1.414 1.415-2.121 2.121-1.414-1.414 2.121-2.121zM5.636 16.95l1.414 1.414-2.121 2.121-1.414-1.414 2.121-2.121zM23 11v2h-3v-2h3zM4 11v2H1v-2h3z")
 </template>
 
