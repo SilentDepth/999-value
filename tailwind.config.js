5227a5306f54c19ca2bf291df398fbbffc84dc07
SilentDepth <silentdepth@outlook.com>
2021-01-28 01:05:17 +0800

    Fix CSS purge issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   purge: [
-    './src/index.html',
+    './index.html',
     './src/**/*.vue',
   ],
   darkMode: 'class',
