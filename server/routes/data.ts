81f95f9ae065d235ea29156ac0789855d6fe21ec
SilentDepth <silentdepth@outlook.com>
2022-05-01 04:52:15 +0800

    use network proxy when dev

--- server/routes/data.ts
+++ server/routes/data.ts
@@ -1,3 +1,5 @@
+import HttpsProxyAgent from 'https-proxy-agent'
+
 const MAX_AGE = 3600000
 let lastCheck = 0
 let data = ''
@@ -6,6 +8,8 @@ export default defineEventHandler(async () => {
   if (Date.now() - lastCheck > MAX_AGE) {
     data = await $fetch('https://currency.world/exchange_rates/all/CNY', {
       headers: { 'Accept-Language': 'zh-CN' },
+      // @ts-ignore
+      agent: process.env.NODE_ENV === 'development' ? new HttpsProxyAgent(process.env.http_proxy) : null,
     })
     lastCheck = Date.now()
   }
